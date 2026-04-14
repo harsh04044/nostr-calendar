94ce23eed6c0d9e830df3d482f36768411a88974
Naman Khandelwal <khandelwalnaman0101@gmail.com>
2026-04-14 15:45:56 +0530

    fix: add parenthesis in Date.now to prevent calendar ID collisions (#99)

--- src/common/calendarList.ts
+++ src/common/calendarList.ts
@@ -194,7 +194,7 @@ export function fetchCalendarLists(
 export async function createCalendar(
   calendarData: Omit<ICalendarList, "id" | "createdAt">,
 ): Promise<ICalendarList> {
-  const idRoot = `${JSON.stringify(calendarData)}-${Date.now}`;
+  const idRoot = `${JSON.stringify(calendarData)}-${Date.now()}`;
   const id = bytesToHex(sha256(utf8ToBytes(idRoot))).substring(0, 30);
   const calendar: ICalendarList = {
     ...calendarData,
